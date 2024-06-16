e2cefe05d3852b2053e320b4eab2e54b2b4ba489
Vishu0106 <kattameedivishnu2003@gmail.com>
2024-06-16 18:23:23 +0530

    setup basic dotenv

---
+++ src/index.js
@@ -0,0 +1,22 @@
+process.loadEnvFile();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {PORT} = require('./config/serverConfig');
+
+const setupAndStartServer = async () => {
+
+    // create the express server
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+    
+    app.listen(PORT, () => {
+        console.log('Server is running on port 3000');
+    });
+    }
+
+    setupAndStartServer();
